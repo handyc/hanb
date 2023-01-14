8742f49a6d6bb69f7e933b934ec5e7234d2db169
Matheus Fillipe <matheusfillipeag@gmail.com>
2023-01-13 23:40:50 -0300

    Work in progress

---
+++ rust/README.md
@@ -0,0 +1,31 @@
+# Rust Implementation of Hanb
+
+## Run
+
+You must have rust and cargo installed.
+
+* Clone this repo:
+
+```
+git clone https://github.com/handyc/hanb.git
+```
+
+* cd into rust
+
+```
+cd rust
+```
+
+* Build
+
+```
+cargo build --release
+```
+
+* Run
+
+```
+./target/release/hanb xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+Enjoy hanb
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,63 @@
+use std::io;
+use std::io::BufRead;
+
+use clap::{self, Parser};
+
+/// Hanb is a simple language for creating model universes at any scale
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Input string of 1 to 64 characters.
+    #[clap(default_value_t = String::from(""))]
+    input: String,
+
+    /// Width of the board
+    #[clap(default_value_t = 12)]
+    width: u8,
+
+    /// Read from stdin instead of using the input argument.
+    #[clap(short, long)]
+    stdin: bool,
+}
+
+fn print_board(board: &str, width: u8) {
+    if board.is_empty() || board.len() > 64 {
+        eprintln!("Input string must be 1 to 64 characters long. You entered with {}", board.len());
+        return;
+    }
+    if width < 10 {
+        eprintln!("Width must be greater than {}", 9);
+        return;
+    }
+    let mut board = board.chars();
+    for c in 0..9 {
+        let mut n_cells = c + 5;
+        if c > 4 {
+            n_cells = 13 - c;
+        }
+        for r in 0..width {
+            if r < (width - n_cells) / 2 || r >= (width + n_cells) / 2 {
+                print!("▗");
+            } else {
+                if let Some(cell) = board.next() {
+                    print!("{}", cell);
+                } else {
+                    print!("▗");
+                }
+            }
+        }
+        println!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if args.stdin {
+        println!("Reading from stdin...");
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            print_board(&line.unwrap(), args.width);
+        }
+    }
+    print_board(&args.input, args.width);
 }
