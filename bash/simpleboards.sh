9d78ed92fbab9f14e990f518b8524d064c5460c7
Christopher Handy <handyca@mcmaster.ca>
2023-01-27 16:35:14 +0100

    Add files via upload
    simple text boards for hanb with two rotation options

---
+++ bash/simpleboards.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+edgeboard="
+           a   a   a   a   a
+         a   a   a   a   a   a
+       a   a   a   a   a   a   a
+     a   a   a   a   a   a   a   a
+   a   a   a   a   a   a   a   a   a
+     a   a   a   a   a   a   a   a
+       a   a   a   a   a   a   a
+         a   a   a   a   a   a
+           a   a   a   a   a"
+pointboard="
+                               a                    
+                          a         a               
+                     a         a         a          
+                a         a         a         a     
+           a         a         a         a         a
+                a         a         a         a     
+           a         a         a         a         a
+                a         a         a         a     
+           a         a         a         a         a
+                a         a         a         a     
+           a         a         a         a         a
+                a         a         a         a     
+           a         a         a         a         a
+                a         a         a         a     
+                     a         a         a          
+                          a         a               
+                               a                   
+"
+
+echo "$edgeboard"
+echo "$pointboard"
